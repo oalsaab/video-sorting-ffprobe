9387f6b9e157d849198ed0b0dcdeb768151882c0
oalsaab <omaralsaab96@gmail.com>
2023-09-29 15:53:22 +0100

    Populated new entry point sorter with skeleton logic for commands

--- sorter.py
+++ sorter.py
@@ -1,1 +1,20 @@
+from pathlib import Path
+
 import click
+
+directory = click.argument("directory", type=click.Path(exists=True))
+extension = click.argument("extension", nargs=-1)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command("audio")
+@directory
+@extension
+def sort_audio(directory: Path, extension: tuple):
+    """Sort by audio."""
+
+    return directory
