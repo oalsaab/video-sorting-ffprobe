cebf153798e5d4dce304f962ca122c16f08b8c83
oalsaab <omaralsaab96@gmail.com>
2023-09-29 12:53:22 +0100

    Refactored reading of stream function name to include private prefix and to prevent clash with built-in python read function

--- utils/create_stream.py
+++ utils/create_stream.py
@@ -6,7 +6,7 @@ import shlex
 COMMAND = "ffprobe -v quiet -print_format json -show_format -show_streams"
 
 
-async def read(file, limit):
+async def _read(file, limit):
     cmd = shlex.split(COMMAND)
     cmd.append(file)
 
@@ -23,7 +23,7 @@ async def read(file, limit):
 async def stream_collection(files):
     limit = os.cpu_count()
 
-    tasks = [asyncio.create_task(read(file, limit)) for file in files]
+    tasks = [asyncio.create_task(_read(file, limit)) for file in files]
     streams = await asyncio.gather(*tasks)
 
     return dict(zip(files, streams))
