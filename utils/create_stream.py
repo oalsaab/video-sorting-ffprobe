9643c007420bc18b8424443afdf88248f56c5a4c
oalsaab <omaralsaab96@gmail.com>
2023-09-29 21:42:25 +0100

    Rewrite of creating streams to simplify logic, returning instance of Stream dataclass instead of NamedTuple map of file with Stream object

--- utils/create_stream.py
+++ utils/create_stream.py
@@ -12,12 +12,12 @@ from utils.read_stream import Stream
 COMMAND = "ffprobe -v quiet -print_format json -show_format -show_streams"
 
 
-class StreamMap(NamedTuple):
-    path: Path
-    stream: Stream
+class Map(NamedTuple):
+    file: Path
+    deserialized: dict
 
 
-async def _read(file: Path, limit: int) -> dict:
+async def _read(file: Path, limit: int) -> Map:
     cmd = shlex.split(COMMAND)
     cmd.append(file)
 
@@ -28,28 +28,33 @@ async def _read(file: Path, limit: int) -> dict:
 
         stdout, _ = await process.communicate()
 
-        return json.loads(stdout)
+        deserialized = json.loads(stdout)
+
+        # Can't pass imported Stream dataclass to async context, event loop closed before import
+        return Map(file, deserialized)
 
 
-async def _stream_collection(files: Iterable[Path]) -> Iterable[tuple[Path, dict]]:
+async def _stream_collection(files: Iterable[Path]) -> list[Map]:
     limit = os.cpu_count()
 
     tasks = [asyncio.create_task(_read(file, limit)) for file in files]
     streams = await asyncio.gather(*tasks)
 
-    return zip(files, streams)
+    return streams
 
 
 def _filter_extensions(directory: Path, extension: tuple) -> Iterable[Path]:
+    extensions = [("." + ext) for ext in extension]
+
     for file in directory.iterdir():
-        if file.suffix.lower() in extension:
+        if file.suffix.lower() in extensions:
             yield file
 
 
-def create_streams(directory: Path, extension: tuple) -> Iterable[StreamMap]:
+def create_streams(directory: Path, extension: tuple) -> Iterable[Stream]:
     filtered = _filter_extensions(directory, extension)
 
     streams = asyncio.run(_stream_collection(filtered))
 
-    for path, stream in streams:
-        yield StreamMap(path=path, stream=Stream(stream))
+    for stream in streams:
+        yield Stream(stream.deserialized, stream.file)
