36f1f66d0cdc6a8e2d74b83fa84a7d8c7a812323
oalsaab <omaralsaab96@gmail.com>
2023-12-02 00:03:47 +0000

    Refactored entry point script to use structural pattern matching instead of conditional statements for readability & added class to build input args object to access passed in option

--- src/scripts/sorter.py
+++ src/scripts/sorter.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import datetime
 from pathlib import Path
 from typing import Iterable
@@ -10,6 +11,19 @@ from .utils.enums import Command
 from .utils.enums import Option
 from .utils.read_stream import Stream
 from .utils.sort_stream import *
+
+
+class Args:
+    def __init__(self):
+        self._args = {idx: arg for idx, arg in enumerate(sys.argv)}
+
+    @property
+    def option(self) -> Optional[str]:
+        return self._args.get(2)
+
+    def err_msg(self, command: str, options: list[str]) -> str:
+        return f"{command} command requires at least one of the following options: {options}"
+
 
 directory = click.argument(
     Argument.DIRECTORY, type=click.Path(exists=True, path_type=Path)
@@ -77,20 +91,22 @@ def duration(
 ):
     """Sort by duration"""
 
-    if not any((longer, shorter, between)):
-        raise click.UsageError(
-            f"Duration command requires at least one of following options: {Option.duration()}"
-        )
+    args = Args()
 
     for stream in streams:
-        if longer:
-            sort_duration_long(stream, longer)
+        match args.option:
+            case Option.LONGER:
+                sort_duration_long(stream, longer)
 
-        if shorter:
-            sort_duration_short(stream, shorter)
+            case Option.SHORTER:
+                sort_duration_short(stream, shorter)
 
-        if between:
-            sort_duration_between(stream, between)
+            case Option.BETWEEN:
+                sort_duration_between(stream, between)
+
+            case _:
+                message = args.err_msg("Duration", Option.duration())
+                raise click.UsageError(message)
 
 
 @cli.command(Command.DATE)
@@ -110,21 +126,24 @@ def creation(
 ):
     """Sort by date"""
 
+    args = Args()
+
     for stream in streams:
-        if year:
-            sort_year(stream, year.year)
+        match args.option:
+            case Option.YEAR:
+                sort_year(stream, year.year)
 
-        elif month:
-            sort_month(stream, month.month)
+            case Option.MONTH:
+                sort_month(stream, month.month)
 
-        elif day:
-            sort_day(stream, day.day)
+            case Option.DAY:
+                sort_day(stream, day.day)
 
-        elif specific:
-            sort_specific_date(stream, specific.date())
+            case Option.SPECIFIC:
+                sort_specific_date(stream, specific.date())
 
-        else:
-            sort_full_date(stream)
+            case _:
+                sort_full_date(stream)
 
 
 @cli.command(Command.SIZE)
@@ -142,17 +161,19 @@ def size_larger(
 ):
     """Sort by size"""
 
-    if not any((larger, smaller, between)):
-        raise click.UsageError(
-            f"Size command requires at least one of following options: {Option.size()}"
-        )
+    args = Args()
 
     for stream in streams:
-        if larger:
-            sort_size_larger(stream, larger)
+        match args.option:
+            case Option.LARGER:
+                sort_size_larger(stream, larger)
 
-        if smaller:
-            sort_size_smaller(stream, smaller)
+            case Option.SMALLER:
+                sort_size_smaller(stream, smaller)
 
-        if between:
-            sort_size_between(stream, between)
+            case Option.BETWEEN:
+                sort_size_between(stream, between)
+
+            case _:
+                message = args.err_msg("Size", Option.size())
+                raise click.UsageError(message)
